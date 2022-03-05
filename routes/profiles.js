541210c3a65fb94b6780d2b84a994fa6c1f3e262
Darnell Dugger <darnelldugger@gmail.com>
2022-03-04 21:21:01 -0500

    add specific profile view, success.

--- routes/profiles.js
+++ routes/profiles.js
@@ -5,6 +5,7 @@ import * as profilesCtrl from '../controllers/profiles.js'
 
 
 router.get('/', isLoggedIn, profilesCtrl.index )
+router.get('/:id', isLoggedIn, profilesCtrl.show)
 
 export {
   router
