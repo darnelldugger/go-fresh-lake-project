e79ecd4b02d1f899cde44cb95f5ee5852004da8f
Darnell Dugger <darnelldugger@gmail.com>
2022-03-06 15:02:24 -0500

    finish adding personal best to profile, successful

--- models/profile.js
+++ models/profile.js
@@ -2,9 +2,23 @@ import mongoose from 'mongoose'
 
 const Schema = mongoose.Schema
 
+const pbSchema = new Schema ({
+  name: {
+    type: String,
+  },
+  weight: {
+    type: Number,
+    min:0,
+    max:100
+  }
+}, {
+  timestamps: true
+})
+
 const profileSchema = new mongoose.Schema({
   name: String,
   avatar: String,
+  personalBest: [pbSchema],
   pisces: [{type: Schema.Types.ObjectId, ref: 'Fish'}],
   lagoon: [{type: Schema.Types.ObjectId, ref: 'Lake'}]
 }, {
