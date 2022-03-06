42a4d150cb7a7ed5622499072655a3bbf36125d3
Darnell Dugger <darnelldugger@gmail.com>
2022-03-06 14:10:47 -0500

    finish add lake visit to profile, complete

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -1,4 +1,5 @@
 import { Profile } from '../models/profile.js'
+import { Lake } from '../models/lake.js'
 import { Fish } from '../models/fish.js'
 
 function index(req, res) {
@@ -17,24 +18,40 @@ function index(req, res) {
 
 function show(req, res) {
   Profile.findById(req.params.id)
+  .populate('lagoon')
+  .populate('pisces')
   .then((profile) => {
     Profile.findById(req.user.profile._id)
     .then(self => {
       const isSelf = self._id.equals(profile._id)
-      res.render("profiles/show", {
-        title: `Angler ${profile.name}'s profile`,
-        profile,
-        isSelf,
+      Lake.find({_id: {$nin: profile.lagoon}}, function(err, lakes) {
+        res.render("profiles/show", {
+          title: `Angler ${profile.name}'s profile`,
+          profile,
+          isSelf,
+          lakes,
+      })
       })
     })
   })
+
   .catch((err) => {
     console.log(err)
     res.redirect("/")
   })
 }
 
+function addLakeVisit(req, res){
+  Profile.findById(req.params.id, function(err, profile) {
+    profile.lagoon.push(req.body.lakeId)
+    profile.save(function(err) {
+      res.redirect(`/profiles/${profile._id}`)
+    })
+  })
+}
+
 export {
   index,
   show,
+  addLakeVisit,
 }
--- routes/profiles.js
+++ routes/profiles.js
@@ -1,11 +1,12 @@
 import { Router } from 'express'
-const router = Router()
-import { isLoggedIn } from '../middleware/middleware.js'
 import * as profilesCtrl from '../controllers/profiles.js'
+import { isLoggedIn } from '../middleware/middleware.js'
 
+const router = Router()
 
 router.get('/', isLoggedIn, profilesCtrl.index )
 router.get('/:id', isLoggedIn, profilesCtrl.show)
+router.post('/:id/lakes', isLoggedIn, profilesCtrl.addLakeVisit)
 
 export {
   router
